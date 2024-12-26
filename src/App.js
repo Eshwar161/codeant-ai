8541e1be4e8977481fd44787a81c73411ed4bdbb
Eshwar <eshwarballa5@gmail.com>
2024-12-26 19:41:28 +0530

    codeantai assignment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './components/Home';
+import Signin from './login/Signin'
+
+function App() {
+  return (
+    <>
+<Home/>
+<Signin/>
+</>
+  );
+}
+
+export default App;
